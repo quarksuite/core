78557e892379612fc79132b9112596aac0ed5259
Chatman R. Jr <crjr.code@protonmail.com>
2021-05-27 10:42:06 -0400

    Update color_scale.js tests.

--- tests/color_scale_test.js
+++ tests/color_scale_test.js
@@ -7,34 +7,34 @@ import {
 } from "https://deno.land/std@0.86.0/testing/asserts.ts";
 
 Deno.test("SPEC tints: can generate tints", function () {
-  assertEquals(tints(2, 100, "red"), ["#ffb4b4", "#ffffff"]);
-  assertEquals(tints(3, 100, "lime"), ["#92ff92", "#d1ffd1", "#ffffff"]);
+  assertEquals(tints(2, 100, "red"), ["#ffa191", "#ffffff"]);
+  assertEquals(tints(3, 100, "lime"), ["#8cff84", "#caffc5", "#ffffff"]);
   assertEquals(tints(4, 100, "blue"), [
-    "#8080ff",
-    "#b4b4ff",
-    "#ddddff",
+    "#306fff",
+    "#73a4ff",
+    "#b8d2ff",
     "#ffffff",
   ]);
 });
 
 Deno.test("SPEC tones: can generate tones", function () {
-  assertEquals(tones(2, 100, "red"), ["#ca5b5b", "#808080"]);
-  assertEquals(tones(3, 100, "lime"), ["#4add4a", "#69b469", "#808080"]);
+  assertEquals(tones(2, 100, "red"), ["#c66356", "#808080"]);
+  assertEquals(tones(3, 100, "lime"), ["#5ed558", "#77ab72", "#808080"]);
   assertEquals(tones(4, 100, "blue"), [
-    "#4040e6",
-    "#5b5bca",
-    "#6f6fa9",
+    "#144be2",
+    "#3b64c4",
+    "#5e75a4",
     "#808080",
   ]);
 });
 
 Deno.test("SPEC shades: can generate shades", function () {
-  assertEquals(shades(2, 100, "red"), ["#b40000", "#000000"]);
-  assertEquals(shades(3, 100, "lime"), ["#00d100", "#009200", "#000000"]);
+  assertEquals(shades(2, 100, "red"), ["#630000", "#000000"]);
+  assertEquals(shades(3, 100, "lime"), ["#009400", "#003600", "#000000"]);
   assertEquals(shades(4, 100, "blue"), [
-    "#0000dd",
-    "#0000b4",
-    "#000080",
+    "#0000ae",
+    "#000063",
+    "#000022",
     "#000000",
   ]);
 });
