0bb59c9053b022be0fff5480bc5f54e03d27b247
Chatman R. Jr <crjr.code@protonmail.com>
2022-03-12 18:31:05 -0500

    Add fn_propagate() functional utility.
    This utility performs the same function as $_ and $$_ propgation methods of the
    object factory. However, because it does not have to track state and the emitter
    is passed in directly, it doesn't have to guess anything about the data it
    consumes.
    
    Also, because it's a functional action, it can be preset and converted to an
    emitter itself. This makes it eligible for including in a process or pipeline.

--- v2/fn.js
+++ v2/fn.js
@@ -1,6 +1,5 @@
 // fn_process Implementation
 
-
 // [[file:../Notebook.org::*fn_process Implementation][fn_process Implementation:1]]
 export function fn_process(...emitters) {
   return compose(...emitters);
@@ -8,7 +7,6 @@ export function fn_process(...emitters) {
 // fn_process Implementation:1 ends here
 
 // fn_preset Implementation
-
 
 // [[file:../Notebook.org::*fn_preset Implementation][fn_preset Implementation:1]]
 export function fn_preset(action, y) {
@@ -18,15 +16,21 @@ export function fn_preset(action, y) {
 
 // fn_pipeline Implementation
 
-
 // [[file:../Notebook.org::*fn_pipeline Implementation][fn_pipeline Implementation:1]]
 export function fn_pipeline(x, ...processes) {
   return compose(...processes)(x);
 }
 // fn_pipeline Implementation:1 ends here
 
-// Composition Internals
+// fn_propagate Implementation
 
+// [[file:../Notebook.org::*fn_propagate Implementation][fn_propagate Implementation:1]]
+export function fn_propagate(emitter, xs) {
+  return xs.map((x) => emitter(x));
+}
+// fn_propagate Implementation:1 ends here
+
+// Composition Internals
 
 // [[file:../Notebook.org::*Composition Internals][Composition Internals:1]]
 function compose(...fns) {
